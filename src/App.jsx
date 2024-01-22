f976fc2116308d289e818a186637c88140f1d547
omarjafor <omarjaforchy@gmail.com>
2024-01-22 21:38:57 +0600

    redux tolkit learning starting

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold'>Redux Tolkit App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
