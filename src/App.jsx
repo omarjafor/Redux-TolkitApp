4c5d7f7e0b7e955f7667164f7b0fe0cab6633341
omarjafor <omarjaforchy@gmail.com>
2024-01-22 21:47:12 +0600

    redux tolkit some update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ function App() {
     <>
       <div>
         <h1 className='font-bold'>Redux Tolkit App</h1>
+        <div className="content">
+          <div className="admin-table">
+            <div className="admin-subtitle">List of User Details</div>
+            <button className="btn add-btn">Add New Users</button>
+          </div>
+          <ul>
+            <li>Hi 1</li>
+          <li>Hii 2</li>
+          </ul>
+          <hr />
+          <div>DeleteAllUser</div>
+        </div>
       </div>
     </>
   )
